70abd7220816b8cd11210ffbee27cb6f394d5fab
azavyalov <alexeysorsa@gmail.com>
2020-10-08 21:27:58 +0300

    Fix bug when notifications are not appeared on API 26+

--- app/src/main/java/com/azavyalov/photogallery/PollService.java
+++ app/src/main/java/com/azavyalov/photogallery/PollService.java
@@ -3,11 +3,14 @@ package com.azavyalov.photogallery;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -59,19 +62,36 @@ public class PollService extends IntentService {
             String CHANNEL_ID = "channel_1";
             Resources resources = getResources();
             Intent i = PhotoGalleryActivity.newIntent(this);
-            PendingIntent pendingIntent = PendingIntent
-                    .getActivity(this, 0, i, 0);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
 
-            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setTicker(resources.getString(R.string.new_pictures_title))
-                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
-                    .setContentTitle(resources.getString(R.string.new_pictures_title))
-                    .setContentText(resources.getString(R.string.new_pictures_text))
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true).build();
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-            notificationManager.notify(0, notification);
+            Notification.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                builder = new Notification
+                        .Builder(this, CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                        .setContentTitle(resources.getString(R.string.new_pictures_title))
+                        .setContentText(resources.getString(R.string.new_pictures_text))
+                        .setContentIntent(pi)
+                        .setAutoCancel(true);
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                        getString(R.string.new_pictures_title), NotificationManager.IMPORTANCE_DEFAULT);
+                assert mNotificationManager != null;
+                mNotificationManager.createNotificationChannel(channel);
+
+            } else {
+                builder = new Notification
+                        .Builder(this)
+                        .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                        .setContentTitle(resources.getString(R.string.new_pictures_title))
+                        .setContentText(resources.getString(R.string.new_pictures_text))
+                        .setContentIntent(pi)
+                        .setAutoCancel(true);
+            }
+
+            mNotificationManager.notify(0, builder.build());
         }
 
         QueryPreferences.setLastResultId(this, resultId);
